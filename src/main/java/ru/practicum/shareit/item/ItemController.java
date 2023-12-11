15068784da8dfba8aee6d7cca8d947e4d61e5a72
AndreyAvdoshin <darkxbird@gmail.com>
2023-12-12 00:02:28 +0900

    feat: Добавить модели и Dto

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,7 +1,10 @@
 package ru.practicum.shareit.item;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.item.dto.ItemDto;
 
 /**
  * TODO Sprint add-controllers.
@@ -9,4 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ItemDto> getAll() {
+        return null;
+    }
 }
---
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -0,0 +1,14 @@
+package ru.practicum.shareit.user.model;
+
+import lombok.Builder;
+import lombok.Data;
+/**
+ * TODO Sprint add-controllers.
+ */
+@Data
+@Builder
+public class User {
+    Long id;
+    String name;
+    String email;
+}
