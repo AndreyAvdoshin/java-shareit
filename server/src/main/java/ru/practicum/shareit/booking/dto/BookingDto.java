60df449cb5c5c851660b6ca2fb121872d3865716
AndreyAvdoshin <darkxbird@gmail.com>
2024-02-03 22:28:17 +0900

    feat: Разбить приложение на сервисы

---
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -0,0 +1,32 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+/**
+ * TODO Sprint add-bookings.
+ */
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingDto {
+
+    private Long itemId;
+
+    @NotNull
+    @FutureOrPresent
+    private LocalDateTime start;
+
+    @NotNull
+    @Future
+    private LocalDateTime end;
+}
